8cb15cfaee4a0fb8c542b3a9178649186a2f8c1d
Murphies Law <emilach82@gmail.com>
2026-09-07 23:32:48 +0200

    feat(web): bootstrap bearer session before app requests

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { bootstrapApiSession } from "./auth/session";
 import "./index.css";
+
+bootstrapApiSession();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
